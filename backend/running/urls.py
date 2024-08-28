ddd5d39523b8544fc109bea8ae467836520c7ec9
sam-kakengi <skakengi@hotmail.co.uk>
2024-08-28 16:12:19 +0100

    Cleared API related files and made migrations

--- backend/running/urls.py
+++ backend/running/urls.py
@@ -1,7 +1,5 @@
+from django.contrib import admin
 from django.urls import path
-from . import views
 
 urlpatterns = [
-    path('activities/', views.RunActivityListCreate.as_view(), name='run_activity_list_create'),
-    path('routes/', views.RoutesListCreate.as_view(), name='routes_list_create'),
 ]
