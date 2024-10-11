c976d77bd4560bebb51327f8bb42a5b2fda83099
Richard-Otterli <99365919+Richard-Otterli@users.noreply.github.com>
2024-10-11 21:50:01 +0100

    Fixing duration formatting on GET Runs

--- backend/running/serializers.py
+++ backend/running/serializers.py
@@ -27,7 +27,7 @@ class GetRunningSerializer(ModelSerializer):
         return GetRouteSerializer(obj.route).data
 
     def get_duration_formatted(self, obj):
-        return format_duration(obj.duration)
+        return format_seconds(obj.duration)
 
     def get_date_formatted(self, obj):
         if obj.finished is None:
