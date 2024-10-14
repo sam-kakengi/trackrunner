53e550f55b60755e1dbc0e8f870d2be31f31f6e5
Richard-Otterli <99365919+Richard-Otterli@users.noreply.github.com>
2024-10-14 20:26:06 +0100

    Postgres integration

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,5 +1,7 @@
 import os
 from dotenv import load_dotenv
+import dj_database_url
+import sys
 
 
 """
@@ -141,11 +143,10 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'), ssl_require=True),
 }
+if 'test' in sys.argv:
+    DATABASES['default'] = dj_database_url.config(default=os.getenv('TEST_DATABASE_URL'), ssl_require=True)
 
 
 # Password validation
