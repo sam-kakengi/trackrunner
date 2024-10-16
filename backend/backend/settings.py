c1df5c2cdf85b7517ef1cf67f12e43f85bab5da1
Richard-Otterli <99365919+Richard-Otterli@users.noreply.github.com>
2024-10-16 21:00:19 +0100

    Settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -147,10 +147,7 @@ DATABASES = {
         'USER': os.getenv('DATABASE_USER'),
         'PASSWORD': os.getenv('DATABASE_PASSWORD'),
         'HOST': os.getenv('DATABASE_HOST'),
-        'PORT': os.getenv('DATABASE_PORT', '5432'),
-        'OPTIONS': {
-                'sslmode': 'require',
-            }
+        'PORT': os.getenv('DATABASE_PORT', '5432')
     }
 }
 # if 'test' in sys.argv:
