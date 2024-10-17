a864f530e838c5c955558afe626f6bfbc249d7a8
Richard-Otterli <99365919+Richard-Otterli@users.noreply.github.com>
2024-10-17 23:06:03 +0100

    Allowed hosts

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False')
 
-ALLOWED_HOSTS = ['trackrunner.run']
+ALLOWED_HOSTS = ['trackrunner.run', 'localhost', '127.0.0.1']
 
 APPEND_SLASH = True
 
